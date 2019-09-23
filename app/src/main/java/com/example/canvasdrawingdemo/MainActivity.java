2670ac482d229ffda204e147ac82eefb1cf972b7
simranbal04 <simranbal01@gmail.com>
2019-09-23 11:21:19 -0400

    background, canvas,

--- app/src/main/java/com/example/canvasdrawingdemo/MainActivity.java
+++ app/src/main/java/com/example/canvasdrawingdemo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.canvasdrawingdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //make our variables for drawing
+        ImageView imageView = findViewById(R.id.imageview);
+        Bitmap b = Bitmap.createBitmap(300, 500, Bitmap.Config.ARGB_8888);
+
+        // SETUP THE  CANVAS
+        Canvas canvas = new Canvas(b);
+
+        //setup yoyur paintbrush
+        Paint paintbrush = new  Paint();
+
+        //draw somestuff on the canvas
+        //set the background color
+        canvas.drawColor(Color.YELLOW);
+
+        //
+
+        imageView.setImageBitmap(b);
     }
 }
