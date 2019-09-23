2cb337ddafca48afa25a9adc29eae7e82d642648
simranbal04 <simranbal01@gmail.com>
2019-09-23 11:39:46 -0400

    SET SIZE TEXT, COLOR

--- app/src/main/java/com/example/canvasdrawingdemo/MainActivity.java
+++ app/src/main/java/com/example/canvasdrawingdemo/MainActivity.java
@@ -28,19 +28,45 @@ public class MainActivity extends AppCompatActivity {
 
         //draw somestuff on the canvas
         //set the background color
-        canvas.drawColor(Color.BLACK);
+        canvas.drawColor(Color.YELLOW);
 
         //CHOOSE A GREEN COLOR
 
         paintbrush.setColor(Color.GREEN);
 
         // draw a line
+        paintbrush.setColor(Color.BLUE);
         canvas.drawLine(10,50,200,50,paintbrush);
         //2ND LINE
         paintbrush.setColor(Color.RED);
-        canvas.drawLine(10,50,200,50,paintbrush);
+        canvas.drawLine(90,90,200,50,paintbrush);
 
         //canvas .drawRect()
+
+        //DRAW some squares
+
+        paintbrush.setColor(Color.WHITE);
+        canvas.drawRect(100,100,120,120, paintbrush);
+
+        //draw a  50*50 square
+        canvas.drawRect(150,150,200,200,paintbrush);
+
+
+
+        //text size
+
+        //1. set the text size
+        paintbrush.setTextSize(40);
+
+        //draw text onto screen
+        canvas.drawText("hello world", 10, 400,paintbrush );
+
+        // draw some more text
+        paintbrush.setTextSize(10);
+        paintbrush.setColor(Color.RED);
+        canvas.drawText("GOODBYE WORLD", 20, 450,paintbrush);
+
+
 
         imageView.setImageBitmap(b);
     }
